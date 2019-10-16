07ec78c816b0f2564e35ea0db1a615ca3c1c674d
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-10-15 17:43:35 -0700

    index fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,39 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var passport = require("passport");
-var dotenv = require("dotenv");
-var util = require("util");
-var url = require("url");
-var querystring = require("querystring");
-var db = require("../models/index");
+var passport = require('passport');
+var dotenv = require('dotenv');
+var util = require('util');
+var url = require('url');
+var querystring = require('querystring');
+var db = require('../models/index');
 dotenv.config();
 
 // Performthelogin, after login Auth0 will redirect to callback
 router.get(
-  "/login",
-  passport.authenticate("auth0", {
-    scope: "openid email profile"
+  '/login',
+  passport.authenticate('auth0', {
+    scope: 'openid email profile'
   }),
-  function(req, res) {
-    console.log("What?");
-    res.redirect("/");
+  function (req, res) {
+    console.log('What?');
+    res.redirect('/');
   }
 );
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
-router.get("/callback", function(req, res, next) {
-  passport.authenticate("auth0", function(err, user, info) {
+router.get('/callback', function (req, res, next) {
+  passport.authenticate('auth0', function (err, user, info) {
     if (err) {
-      console.log("HEROKY");
+      console.log('HEROKY');
       return next(err);
     }
     if (!user) {
-      console.log("HEY....");
-      return res.redirect("/login");
+      console.log('HEY....');
+      return res.redirect('/login');
     }
-    req.logIn(user, function(err) {
+    req.logIn(user, function (err) {
       if (err) {
         return next(err);
       }
@@ -44,25 +44,25 @@ router.get("/callback", function(req, res, next) {
         where: {
           auth_id: user.user_id
         }
-      }).then(function(dbUser) {
-        console.log("USER:", dbUser);
-        dbUser.length === 0 ? db.User.create({ auth_id: user.user_id, email: user.displayName }).then(function(crap) { res.redirect(returnTo || "/user"); }) : res.redirect(returnTo || "/user");
+      }).then(function (dbUser) {
+        console.log('USER:', dbUser);
+        dbUser.length === 0 ? db.User.create({ auth_id: user.user_id, email: user.displayName }).then(function (crap) { res.redirect(returnTo || '/user'); }) : res.redirect(returnTo || '/user');
       });
     });
   })(req, res, next);
 });
 
 // Perform session logout and redirect to homepage
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
 
-  var returnTo = req.protocol + "://" + req.hostname;
+  var returnTo = req.protocol + '://' + req.hostname;
   var port = req.connection.localPort;
   if (port !== undefined && port !== 80 && port !== 443) {
-    returnTo += ":" + port;
+    returnTo += ':' + port;
   }
   var logoutURL = new url.URL(
-    util.format("https://%s/logout", process.env.AUTH0_DOMAIN)
+    util.format('https://%s/logout', process.env.AUTH0_DOMAIN)
   );
   var searchString = querystring.stringify({
     client_id: process.env.AUTH0_CLIENT_ID,
